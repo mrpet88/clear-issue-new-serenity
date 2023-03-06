573ecda9b44ec1aa88b22cdbad6d416a6f0d0b31
marinos.petratos <marinos.petratos@leaseplan.com>
2023-03-06 20:16:54 +0100

    added amazon step

--- src/test/java/starter/navigation/WikipediaHomePage.java
+++ src/test/java/starter/navigation/WikipediaHomePage.java
@@ -3,7 +3,8 @@ package starter.navigation;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-//@DefaultUrl("https://www.netflix.com/ie/")
+//@DefaultUrl("https://www.amazon.nl/")
 @DefaultUrl("https://www.netflix.com/ie/")
 public class WikipediaHomePage extends PageObject {
 }
+
--- src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
@@ -1,22 +1,15 @@
 package starter.stepdefinitions;
 
-import com.microsoft.playwright.BrowserType;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.ensure.Ensure;
-import net.serenitybdd.screenplay.playwright.Target;
-import net.serenitybdd.screenplay.playwright.abilities.BrowseTheWebWithPlaywright;
-import net.serenitybdd.screenplay.playwright.interactions.Enter;
-import net.serenitybdd.screenplay.playwright.interactions.Open;
 import org.openqa.selenium.By;
 import starter.navigation.NavigateTo;
-import starter.search.WikipediaArticle;
 
 public class SearchStepDefinitions extends UIInteractionSteps {
-    private final By inputSearch = By.name("email");
+    private final By inputSearchNetFlix = By.name("email");
+    private final By inputSearchNetAmazon = By.name("field-keywords");
 
     @Given("{actor} is researching things on the internet")
     public void researchingThings(Actor actor) {
@@ -24,13 +17,24 @@ public class SearchStepDefinitions extends UIInteractionSteps {
     }
 
     @When("he looks up {string}")
-    public void searchesFor(String term) {
+    public void searchesForNetflix(String term) {
         for (int i = 0; i <= 7; i++) {
 //            Selenium implementation
-            $(inputSearch).clear();
-            $(inputSearch).sendKeys(term);
+            $(inputSearchNetFlix).clear();
+            $(inputSearchNetFlix).sendKeys(term);
 //            Serenity Implementation
-//            $(inputSearch).type(term);
+//            $(inputSearchNetFlix).type(term);
         }
     }
+
+//    @When("he looks up {string}")
+//    public void searchesForAmazon(String term) {
+//        for (int i = 0; i <= 7; i++) {
+////            Selenium implementation
+//            $(inputSearchNetAmazon).clear();
+//            $(inputSearchNetAmazon).sendKeys(term);
+////            Serenity Implementation
+////            $(inputSearchNetAmazon).type(term);
+//        }
+//    }
 }
